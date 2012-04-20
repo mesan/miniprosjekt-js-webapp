b8ea8e54ac36277a5491c117fbde1382dc82d88b
larskrj <larsj@mesan.no>
2012-04-20 14:40:10 +0200

    Lagt til jasmine specs

---
+++ knockout/PersonRegister/PersonRegister/Scripts/tests/ApiConnection.spec.js
@@ -0,0 +1,29 @@
+﻿define(['ApiConnection', 'sinon'], function (ApiConnection, sinon) {
+
+	describe("ApiConnection", function () {
+
+		beforeEach(function () {
+			this.server = sinon.fakeServer.create();
+		});
+
+		afterEach(function () {
+			this.server.restore();
+		});
+
+		it("should get all as json", function () {
+			var url = "min/tilfeldige/url";
+
+			var apiConnection = new ApiConnection(url);
+
+			this.server.respondWith("GET", url,
+                                 [200, { "Content-Type": "application/json" } ,
+                                 '[{ "id": 12, "name": "Gustav" }]']);
+
+			var callback = sinon.spy();
+			apiConnection.getAll(url, callback);
+			this.server.respond();
+			debugger;
+			expect(callback.calledWith([{ id: 12, name: "Gustav"}])).toBeTruthy();
+		});
+	});
+});
---
+++ knockout/PersonRegister/PersonRegister/Scripts/tests/PersonViewModel.spec.js
@@ -0,0 +1,23 @@
+﻿define(['ViewModels/PersonViewModel', 'sinon'], function (PersonViewModel, sinon) {
+
+	describe("PersonViewModel", function () {
+
+		beforeEach(function () {
+			this.server = sinon.fakeServer.create();
+		});
+
+		afterEach(function () {
+			this.server.restore();
+		});
+
+		it("should have an updatePerson function", function () {
+			var personViewModel = new PersonViewModel();
+			expect(typeof (personViewModel.updatePerson)).toEqual("function");
+		});
+
+		it("should pass", function () {
+			expect("a").toEqual("a");
+		});
+
+	});
+});
